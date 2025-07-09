6502cc5947b667c2e61af1fb654ec0997089e2d8
Shaxshax-Mahat <abdulrahmanmahat0@gmail.com>
2025-07-09 23:11:17 +0300

    Modifies index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// Function expression for divide
+const divide = function() {
+  return 2000 / 100;
+};
+
+// Arrow function for square
+const square = num => num * num;
+
+// Arrow function for add
+const add = (a, b) => a + b;
+
+
